c0859bfead9a56083f613d67df6059b83ac6ad23
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-07-14 14:59:33 -0400

    add chains to wagmi config

--- lib/wagmi.config.ts
+++ lib/wagmi.config.ts
@@ -1,5 +1,14 @@
 import { jbChains } from "@/registry/juicebox/pay-project-form/lib/chains";
-import { arbitrum, base, mainnet, optimism } from "viem/chains";
+import {
+  arbitrum,
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+  mainnet,
+  optimism,
+  optimismSepolia,
+  sepolia,
+} from "viem/chains";
 import { createConfig, fallback, http, injected, Transport } from "wagmi";
 
 const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID;
@@ -10,15 +19,43 @@ export const transports: Record<number, Transport> = {
     http(),
   ]),
   [optimism.id]: fallback([
-    ...(INFURA_ID ? [http(`https://optimism-mainnet.infura.io/v3/${INFURA_ID}`)] : []),
+    ...(INFURA_ID
+      ? [http(`https://optimism-mainnet.infura.io/v3/${INFURA_ID}`)]
+      : []),
     http(),
   ]),
   [base.id]: fallback([
-    ...(INFURA_ID ? [http(`https://base-mainnet.infura.io/v3/${INFURA_ID}`)] : []),
+    ...(INFURA_ID
+      ? [http(`https://base-mainnet.infura.io/v3/${INFURA_ID}`)]
+      : []),
     http(),
   ]),
   [arbitrum.id]: fallback([
-    ...(INFURA_ID ? [http(`https://arbitrum-mainnet.infura.io/v3/${INFURA_ID}`)] : []),
+    ...(INFURA_ID
+      ? [http(`https://arbitrum-mainnet.infura.io/v3/${INFURA_ID}`)]
+      : []),
+    http(),
+  ]),
+  [baseSepolia.id]: fallback([
+    ...(INFURA_ID
+      ? [http(`https://base-sepolia.infura.io/v3/${INFURA_ID}`)]
+      : []),
+    http(),
+  ]),
+  [optimismSepolia.id]: fallback([
+    ...(INFURA_ID
+      ? [http(`https://optimism-sepolia.infura.io/v3/${INFURA_ID}`)]
+      : []),
+    http(),
+  ]),
+  [arbitrumSepolia.id]: fallback([
+    ...(INFURA_ID
+      ? [http(`https://arbitrum-sepolia.infura.io/v3/${INFURA_ID}`)]
+      : []),
+    http(),
+  ]),
+  [sepolia.id]: fallback([
+    ...(INFURA_ID ? [http(`https://sepolia.infura.io/v3/${INFURA_ID}`)] : []),
     http(),
   ]),
 };
